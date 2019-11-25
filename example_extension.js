84590e62990605fef46c4111d19249eaf8903279
Miron Gertis <56720084+datennutzung@users.noreply.github.com>
2019-11-25 15:46:47 +0100

    test thing

--- example_extension.js
+++ example_extension.js
@@ -236,6 +236,19 @@ function initializeButtons() {
 
 function testThings() {
     console.log("Test Things!")
+    tableau.extensions.dashboardContent.dashboard.worksheets.find(w => w.name === "Sale Map").getDataSourcesAsync().then(datasources =>
+        {dataSource = datasources.find(datasource => datasource.name === "Sample - Superstore");
+          return dataSource.getUnderlyingDataAsync();
+         }).then(dataTable => {
+            let field = dataTable.columns.find(column => column.fieldName === "Sub-Category");
+            let list = [];
+            for (let row of dataTable.data) {
+                list.push(row[field.index].value);
+            }
+            let values = list.filter((el, i, arr) => arr.indexOf(el) === i);
+            console.log(values)
+        });
+        
 }
 
 function deleteGroupsTableEntry(rowObject) {
